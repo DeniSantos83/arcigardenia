67299bbd38a9c79003c3ec5b9ece5b1c8fa5b54c
Valdenir Santos <134643293+DeniSantos83@users.noreply.github.com>
2025-11-03 15:30:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Scroll suave nos links da navbar
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
